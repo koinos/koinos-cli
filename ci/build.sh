756a4e5164351f251a74549abb7ab9e8c218bf92
Nathaniel Caldwell <nbcwell@gmail.com>
2021-10-05 15:52:56 -0400

    Add ci

---
+++ ci/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+
+go get ./...
+mkdir build
---
+++ ci/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo gem install coveralls-lcov
+go get -u github.com/jandelgado/gcov2lcov
+go get -u golang.org/x/lint/golint
