a67cff5f9cdfd5af784b8cc5903033de1cca683f
Nathaniel Caldwell <nbcwell@gmail.com>
2021-10-05 15:07:39 -0400

    Fix unit tests

---
+++ internal/wallet/command_test.go
@@ -0,0 +1,47 @@
+package wallet
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+// needed to retrieve requests that arrived at httpServer for further investigation
+var requestChan = make(chan *RequestData, 1)
+
+// the request datastructure that can be retrieved for test assertions
+type RequestData struct {
+	request *http.Request
+	body    string
+}
+
+// set the response body the httpServer should return for the next request
+var responseBody = ""
+
+var httpServer *httptest.Server
+
+// start the testhttp server and stop it when tests are finished
+func TestMain(m *testing.M) {
+	httpServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		// put request and body to channel for the client to investigate them
+		requestChan <- &RequestData{r, string(data)}
+
+		fmt.Fprintf(w, responseBody)
+	}))
+	defer httpServer.Close()
+
+	os.Exit(m.Run())
+}
+
+func TestBalance(t *testing.T) {
+	client := NewKoinosRPCClient(httpServer.URL)
+	client.GetAccountBalance([]byte("ABCDEFG"), []byte{1, 2, 3}, 678)
+	//client.client.CallRaw(request)
+	req := (<-requestChan).body
+	fmt.Printf(req)
+}
